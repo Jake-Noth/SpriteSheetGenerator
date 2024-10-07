09cfd54a41dde1441e39ed15340ca0d054b48f9c
Jake Nothvogel <jacobnothvogel@gmail.com>
2024-10-07 14:38:44 -0500

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,47 @@
-import { useContext } from 'react';
 import './index.css';
-import FileLoad from './components/FileLoad';
-import { VideoContext } from './context/Contex';
-import Canvas from './components/Canvas';
-import Slider from './components/Slider';
-import KeyFrameButton from './components/KeyFrameButton';
-import FPSButtons from './components/FPSButtons';
-import SelectedFrames from './components/SelectedFrames';
-import useResizeHandler from './hooks/ScreenChange';
-import SortFrameByTime from './components/FrameSortButton';
-import ResetButton from './components/ResetButton';
+
 
 function App() {
-  const { videoElement, frames } = useContext(VideoContext);
-  useResizeHandler();
 
   return (
-    <>
-      <h1 id="topHeader">Sprite Gen</h1>
-        
-      <div id="canvas-and-frames">
-        <div id='canvas-section-and-modifiers'>
+    <>  
+      <div id="canvas-and-frames-container">
+        <div id='canvas-and-modifiers-container'>
           
-          <div id='canvas-section'>
-            {videoElement && <Canvas/>}
+          <div id='main-video-canvas-container'>
+            
           </div>
           
           <div id='modifiers-container'>
             <div id='slider-and-frame-button-container'>
+
               <div id='slider-container'>
-                {videoElement && <Slider/>}
+                
               </div>
 
-              <div id='frame-button-container'>
-                {videoElement && <KeyFrameButton/>}
+              <div id='keyframe-button-container'>
+                
               </div> 
             </div>
 
             <div id='modifier-buttons-container'>
-              <FPSButtons/>
-              <SortFrameByTime/>
+              
             </div>
 
             <div id='submit-button-container'>
 
             </div>
-            
-              
-  
           </div>
         </div>
         
-        <div id='frames-and-upload-section'>
-          <div id='file-upload-container'>
-            <FileLoad/>
-          </div>
+        <div id='frames-and-upload-container'>
 
-          <SelectedFrames/>
+          <div id='file-upload-container'>
+           
+          </div>
           
-          <div id='reset-button-div'>
-            <ResetButton/>
+          <div id='reset-button-container'>
+           
           </div>
         </div>
       </div>
