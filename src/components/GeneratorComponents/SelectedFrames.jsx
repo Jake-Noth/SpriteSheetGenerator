af2612908ed36430e7432ec1ed379abbfc2d8502
Jake Nothvogel <jacobnothvogel@gmail.com>
2024-10-15 17:42:35 -0500

    style: added radius to some elements

--- src/components/GeneratorComponents/SelectedFrames.jsx
+++ src/components/GeneratorComponents/SelectedFrames.jsx
@@ -15,20 +15,17 @@ export default function SelectedFrames() {
             const newVideoElement = document.createElement('video')
             newVideoElement.src = videoElement.src
 
-            
-
             newVideoElement.onloadedmetadata = () =>{
                 newVideoElement.currentTime = parseFloat(frameTimes[index].toFixed(8));
                 newVideoElement.onseeked = () => {
                     const ctx = canvas.getContext("2d");
-                    console.log('seekde')
                     const videoAspectRatio = newVideoElement.videoWidth / newVideoElement.videoHeight;
-    
+
                     const parentDiv = canvas.parentElement;
-                    const divHeight = parentDiv.offsetHeight-3;
+                    const divHeight = parentDiv.offsetHeight;
                     const divWidth = parentDiv.offsetWidth;
                     let canvasWidth, canvasHeight;
-    
+
                     if (divWidth / divHeight > videoAspectRatio) {
                         canvasHeight = divHeight;
                         canvasWidth = canvasHeight * videoAspectRatio;
@@ -36,30 +33,28 @@ export default function SelectedFrames() {
                         canvasWidth = divWidth;
                         canvasHeight = canvasWidth / videoAspectRatio;
                     }
-    
-              
+
+                    // Scale down the canvas size by 90%
+                    const scaleFactor = 0.93;
+                    canvasWidth *= scaleFactor;
+                    canvasHeight *= scaleFactor;
+
                     canvas.width = canvasWidth;
                     canvas.height = canvasHeight;
-    
+
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
                     ctx.drawImage(newVideoElement, 0, 0, canvas.width, canvas.height);
                     frameRefs.current[index] = canvas;
-                    
                 };
             }
-            
-            
-            
-            
-        }
-        
+        }   
     };
 
     return (
         <>
             {frameTimes.map((item, index) => (
-                <div className="frame" key={index}>
-                    <canvas ref={(el) => setCanvas(el, index)} />
+                <div className="frame-container" key={index}>
+                    <canvas className = "frame" ref={(el) => setCanvas(el, index)} />
                 </div>
             ))}
         </>
